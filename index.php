b5b700cc06d37f9e4438cd65c244c1bff494d0ae
Enkre <gustave.mehaddi@gmail.com>
2018-11-15 15:57:56 +0100

    finis

--- index.php
+++ index.php
@@ -1,8 +1,14 @@
-<!DOCTYPE html>
 <html>
 <head>
-	<title>Friend book</title>
-	<style>
+
+<header>
+ <h2>Friend book</h2>
+</header>
+
+
+
+<style>
+
 /* Style the header */
 header {
  background-color: #666;
@@ -18,59 +24,55 @@ footer {
  text-align: center;
  color: white;
 }
-p{
-	font-size: 35px;
-
-}
-li{
-	width: 10;
-}
 </style>
 </head>
 
-</head>
+
 <body>
-<header>
- <h2>Friend book</h2>
-</header>
-<br>
-	<form method="post" action="index.php" >
-	 Name:<input type="text" name="name">
-	<input type="submit">
-	</form>
-<br>
 
-<p><b>My Best Friends :</b></p>
+<form action="index.php" method="post">
+<br>Name: <input type="text" name="name">
+<input type="submit">
+</form>
+
 <?php
-	$filename = 'friends.txt';
-	if(isset($_POST['name'])) {
-		$file = fopen( $filename, "a" );
-		fwrite( $file, $_POST['name']);
-		fwrite( $file, "\n");
-	}
+echo "<h1>My best friends: </h1>";
+    
+echo "<ul>";
 
-	$file = fopen( $filename, "r" );
-	while (!feof($file)) {
-	    $ligne=fgets($file);
-	    if(isset($_POST['filter']) and $_POST['filter']!=''){
-	    	$ligne=strstr($ligne, $_POST['filter'],true).strstr($ligne, $_POST['filter']);
-	    	if($ligne)echo "<ul><li>$ligne</ul></li><br>";
-	    }
-	    else{
-	    	if($ligne)echo "<ul><li>$ligne</ul></li><br>";
-	    }
-	    
-	}
-	
+$filename = 'newfile.txt';
+
+$file = fopen( $filename, "r" );
+    
+if( $file != false ) {
+    
+    while(!feof($file)){
+        
+        $friend = fgets($file);
+        if(strlen($friend) > 0) {
+            echo "<li>$friend</li>";
+        }
+    }
+}
+fclose( $file );
+    
+if (isset($_POST['name']) && strlen($_POST['name']) > 0) {
+    $name = $_POST['name'];
+    $file = fopen( $filename, "a" );
+    if($file != false){
+         echo "<b><li>$name</li></b>"; 
+         fwrite( $file, $name . "\n" );
+         fclose( $file );
+    }
+
+}
+echo "</ul>";
+
 ?>
-<br>
-	<form method="post" action="index.php" >
-	<input type="text" name="filter">
-	<input type="submit" value="Filter list">
-	</form>
-<br>
-<footer>
- <h2>footer</h2>
-</footer>
+
 </body>
+
+<footer>
+ <p>Footer</p>
+</footer>
 </html>
